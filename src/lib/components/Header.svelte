e9e943ed73af71e1148e65af18880b20f8b06cfd
mayanhavoc <mayanhavoc@gmail.com>
2023-03-31 18:58:19 -0400

    add mobileNavbar and media queries

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,193 +1,211 @@
 <script>
-    let navVisible = false;
+	import { onMount } from 'svelte';
 
-    function toggleNav(){
-        navVisible = !navVisible;
-    }
+	let isOpen = false;
+	let bar1Class = '';
+	let bar2Class = '';
+	let bar3Class = '';
+	let drawerClass = '';
 
-    function closeNav(){
-        setTimeout(() => {
-            navVisible = false;
-        }, 300)
-    }
+	const toggleHamburger = () => {
+		isOpen = !isOpen;
+		setAnimationClasses();
+	};
+
+	const setAnimationClasses = () => {
+		if (isOpen) {
+			bar1Class = 'animateBar1';
+			bar2Class = 'animateBar2';
+			bar3Class = 'animateBar3';
+			drawerClass = 'openDrawer';
+		} else {
+			bar1Class = '';
+			bar2Class = '';
+			bar3Class = '';
+			drawerClass = '';
+		}
+	};
+
+	onMount(() => {
+		window.addEventListener('click', (event) => {
+			if (!event.target.closest('.hamburger')) {
+				isOpen = false;
+				setAnimationClasses();
+			}
+		});
+	});
 </script>
 
-<style>
-
-    header {
-        padding: 1rem 0;
-        position: fixed;
-        background-color: var(--background-color);
-        font-family: var(--primary-font);
-        font-size: var(--x-small);
-        text-align: center;
-    }
-
-    .nav__container {
-        width: 90%;
-        max-width: 1200px;
-        margin: 0 auto;
-        position: relative;
-        color: var(--highlight-color);
-    }
-
-    .nav {
-        visibility: hidden;
-        height: 0;
-        position: relative;
-        z-index: 0;
-    }
-
-    .nav--visible {
-        visibility: visible;
-        height: auto;
-        position: relative;
-        transition: 0.5s ease-in-out;
-        z-index: 1;
-    }
-
-    .logo {
-        margin-right: 1em;
-    }
-    
-    .nav__list {
-      margin: 0 auto;
-      padding: 1rem 0;
-      list-style: none;
-      gap: 1rem;
-    }
-
-    .nav__item {
-        margin-top: 1rem;
-    }
-    
-    .nav__link {
-      text-decoration: none;
-      color: var(--font-color);
-    }
-
-    .nav__link:hover,
-    .nav__link:focus {
-        opacity: 0.75;
-    }
-
-    .nav-toggle {
-        cursor: pointer;
-        border: 0;
-        border-radius: 100%;
-        width: 3rem;
-        height: 3rem;
-        padding: 0em;
-        transition: opacity 250ms ease;
-        position: absolute;
-        left: 0;
-        background: var(--font-color)
-    }
-
-    .nav-toggle:focus,
-    .nav-toggle:hover {
-        opacity: 0.75;
-    }
-
-    .hamburger {
-        width: 50%;
-        position: relative;
-    }
-
-    .hamburger,
-    .hamburger::before,
-    .hamburger::after {
-        display: block;
-        margin: 0 auto;
-        height: 3px;
-        background: var(--background-color);
-    }
-
-    .hamburger::before,
-    .hamburger::after {
-        content: "";
-        width: 100%;
-    }
-
-    .hamburger::before {
-        transform: translateY(-6px);
-    }
-
-    .hamburger::after {
-        transform: translateY(3px);
-    }
-
-
-    @media (min-width: 800px) { 
-        header {
-            width: 100%;
-        }
-
-        .nav-toggle {
-            display:none;
-        }
-
-        .nav {
-            visibility: visible;
-            display: flex;
-            align-items: center;
-            position: relative;
-            height: auto;
-        }
-
-        .nav__list {
-            display: flex;
-            margin: 0 auto;
-        }
-
-        .nav__item {
-          margin: 0 0 0 1.5em;
-        }
-    
-        .row {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-        }
-    }
-</style>
-
-<header>
-  <div class="nav__container row">
-      <button class="nav-toggle" aria-label="open navigation" on:click={toggleNav}>
-          <span class="hamburger"></span>
-      </button>
-      <a class="logo" href="/">
-        <img src="45b_logo.svg" alt="45B logo">
-        </a>
-      <nav class={navVisible ? 'nav--visible' : 'nav'}>
-          <ul class="nav__list">
-              <li class="nav__item">
-                  <a href="/" class="nav__link" on:click={closeNav}>
-                      Home
-                  </a>
-              </li>
-              <li class="nav__item">
-                  <a href="/about" class="nav__link" on:click={closeNav}>
-                      About
-                  </a>
-              </li>
-              <li class="nav__item">
-                  <a href="/products" class="nav__link" on:click={closeNav}>
-                      Products
-                  </a>
-              </li>
-              <li class="nav__item">
-                  <a href="/solutions" class="nav__link" on:click={closeNav}>
-                      Solutions
-                  </a>
-              </li>
-              <li class="nav__item">
-                  <a href="/contact" class="nav__link" on:click={closeNav}>
-                      Contact
-                  </a>
-              </li>
-          </ul>
-      </nav>
-  </div>
+<header class="navbar">
+	<a class="logo" href="/">
+		<img src="45b_logo.svg" alt="45B logo" />
+	</a>
+	<nav class="right">
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		<a href="/products">Products</a>
+		<a href="/solutions">Solutions</a>
+		<a href="/contact">Contact</a>
+	</nav>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div class="hamburger" on:click={toggleHamburger}>
+		<div class="bar bar1" class:animateBar1={bar1Class} />
+		<div class="bar bar2" class:animateBar2={bar2Class} />
+		<div class="bar bar3" class:animateBar3={bar3Class} />
+	</div>
 </header>
+<nav class="mobileNav" class:openDrawer={drawerClass}>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/products">Products</a>
+	<a href="/solutions">Solutions</a>
+	<a href="/contact">Contact</a>
+</nav>
+
+<style>
+	.navbar {
+		display: flex;
+		justify-content: space-between;
+		padding: 2em 1rem;
+		margin: 0 auto;
+		max-width: 90%;
+		width: 100%;
+	}
+
+	.navbar .left {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		font-size: var(--small);
+		font-family: var(--primary-font);
+		width: 100%;
+	}
+
+	.navbar .left a {
+		text-decoration: none;
+		color: var(--font-color);
+	}
+
+	.navbar .left a:hover {
+		color: var(--highlight-color);
+	}
+
+	.navbar .right {
+		display: none;
+	}
+
+	.navbar .right a {
+		text-decoration: none;
+		color: var(--font-color);
+	}
+
+	.navbar .right a:hover {
+		text-decoration: underline;
+	}
+
+	.navbar .hamburger {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		width: 36px;
+	}
+
+	.navbar .bar {
+		background-color: var(--font-color);
+		height: 3px;
+		width: 100%;
+		margin: 3px;
+	}
+
+	.animateBar1 {
+		animation: flipBar1 0.5s;
+		animation-fill-mode: forwards;
+	}
+	.animateBar2 {
+		animation: fadeBar2 0.5s;
+		animation-fill-mode: forwards;
+	}
+	.animateBar3 {
+		animation: flipBar3 0.5s;
+		animation-fill-mode: forwards;
+	}
+
+	.openDrawer {
+		transform: translateX(-150px);
+		transition: all 1s;
+	}
+
+	.mobileNav {
+		width: 150px;
+		height: 300px;
+		background-color: var(--highlight-color);
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		position: fixed;
+		top: 80px;
+		right: -160px;
+		z-index: 2;
+		padding: 1rem;
+	}
+
+	.mobileNav a {
+		text-decoration: none;
+		color: var(--background-color);
+		font-family: var(--secondary-font);
+		margin-top: 1rem;
+	}
+
+	.mobileNav a:hover {
+		text-decoration: underline;
+	}
+
+	@keyframes fadeBar2 {
+		from {
+			transform: scaleX(1);
+			opacity: 1;
+		}
+		to {
+			transform: scaleX(0);
+			opacity: 0;
+		}
+	}
+
+	@keyframes flipBar1 {
+		from {
+			transform: rotate(0);
+		}
+		to {
+			transform: rotate(45deg) translate(3px, 9px);
+		}
+	}
+	@keyframes flipBar3 {
+		from {
+			transform: rotate(0);
+		}
+		to {
+			transform: rotate(-45deg) translate(3px, -9px);
+		}
+	}
+
+	@media (min-width: 768px) {
+		.navbar .right {
+			width: 25em;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			font-family: var(--secondary-font);
+		}
+
+		.navbar .hamburger {
+			display: none;
+		}
+
+		.mobileNav {
+			display: none;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,126 @@
 <svelte:head>
-    <title>45b.io - Embrace the blockchain</title>
+	<title>45b.io - Embrace the blockchain</title>
 </svelte:head>
 
-<style>
-    .hero {
-        width: 80%;
-        max-width: 1100px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin: 0 auto;
-        padding: 100px 0;
-        position: relative;
-    }
-    
-    .hero__img {
-        width: 32%;
-        align-self: flex-start;
-    }
-    
-    .hero__img--reverse {
-        width: 32%;
-    }
-
-    .align-end {
-        display: flex;
-        flex-direction: column;
-        justify-content: end;
-        align-items: end;
-        text-align:end;
-    }
-</style>
-
 <section class="hero">
-        <div>
-            <h1 class="heading">Embrace the blockchain</h1>
-            <a href="#" class="btn">Get started</a>
-        </div>
-        <img src="hero__img.png" alt="hero image">
+	<img class="hero__img" src="hero__img.png" alt="hero image" />
+	<div class="hero-headline">
+		<h1 class="heading">Embrace the blockchain</h1>
+		<div class="hero-btn">
+			<button class="btn">
+				<a href="#" class="item__link">Get in touch</a>
+			</button>
+		</div>
+	</div>
 </section>
 <section class="container row">
-    <div>
-        <h2 class="heading">Learn</h2>
-        <p class="paragraph">No ID or expensive investments. Install a digital wallet and get $10 Ada or BTC in less than 5 minutes</p>
-    </div>
-    <img src="learn__image.png" alt="learn icon" class="hero__img">
+	<img src="learn__image.png" alt="learn icon" class="hero__img" />
+	<div>
+		<h2 class="heading">Learn</h2>
+		<p class="paragraph">
+			No ID or expensive investments. Install a digital wallet and get $10 Ada or BTC in less than 5
+			minutes
+		</p>
+	</div>
+</section>
+<section class="container row reverse">
+	<img src="manage__image.png" alt="manage icon" class="hero__img" />
+	<div class="align-end">
+		<h2 class="heading heading--reverse">Manage</h2>
+		<p class="paragraph paragraph--reverse">
+			Learn how to manage your crypto portfolio, keep Ada in your balance sheets and accept accept
+			payments in Ada
+		</p>
+	</div>
 </section>
 <section class="container row">
-    <img src="manage__image.png" alt="manage icon" class="hero__img">
-    <div class="align-end">
-        <h2 class="heading">Manage</h2>
-        <p class="paragraph">Learn how to manage your crypto portfolio, keep Ada in your balance sheets and accept accept payments in Ada</p>
-    </div>
-</section>
-<section class="container row">
-    <div>
-        <h2 class="heading">Build</h2>
-        <p class="paragraph">Let us help you manage web3 projects. Create a website with web3 capabilities and integrate solutions through our partners.</p>
-    </div>
-    <img src="build__image.png" alt="build icon" class="hero__img">
+	<img src="build__image.png" alt="build icon" class="hero__img" />
+	<div>
+		<h2 class="heading">Build</h2>
+		<p class="paragraph">
+			Let us help you manage web3 projects. Create a website with web3 capabilities and integrate
+			solutions through our partners.
+		</p>
+	</div>
 </section>
 
+<style>
+	.hero {
+		width: 80%;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: start;
+		align-items: center;
+		margin: 0 auto;
+		position: relative;
+	}
+
+	.hero-headline {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.heading {
+		text-align: center;
+		margin: 2rem 0;
+	}
+
+	.hero-btn {
+		display: block;
+		align-self: center;
+	}
+
+	.hero__img {
+		margin-top: 2rem;
+		width: 100%;
+		max-width: 200px;
+		align-self: center;
+	}
+
+	@media (min-width: 280px) and (max-width: 359px) {
+		.hero {
+			height: 71vh;
+			justify-content: center;
+		}
+	}
+
+	@media (min-width: 390px) {
+		.hero {
+			height: 80vh;
+			justify-content: center;
+		}
+	}
+
+	@media (min-width: 768px) {
+		.hero {
+			flex-direction: row;
+			justify-content: space-between;
+		}
+
+		.hero-headline {
+			margin-top: 2rem;
+			max-width: 50vw;
+		}
+
+		.hero-btn {
+			margin: 0;
+		}
+
+		.hero__img {
+			min-width: 40%;
+		}
+	}
+
+	@media (min-width: 820px) {
+		.hero {
+			height: 90vh;
+		}
+	}
+
+	@media (min-width: 912px) {
+		.hero {
+			height: 83vh;
+		}
+	}
+</style>
